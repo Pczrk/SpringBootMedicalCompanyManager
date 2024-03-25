c1015b77996c6c6d6388526ad4800f052fe78cc7
Pczrk <47p4tryk5@gmail.com>
2024-03-25 22:25:05 +0100

    Initial commit

---
+++ src/test/java/com/example/medicalcliniccompanymanager/MedicalClinicCompanyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicalcliniccompanymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicCompanyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
